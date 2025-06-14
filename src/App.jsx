1a5b48c2f6eca2b05ee27236da4afbdacfc81ebc
ChethanaRamesh012 <122734826+ChethanaRamesh012@users.noreply.github.com>
2025-06-14 15:34:13 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Education from './components/Education';
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -17,6 +18,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <About />
+        <Education/>
         <Experience />
         <Projects />
         <Contact />
